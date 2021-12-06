3cf1092383a0aeecc9dc7a2ad161465cd17d21b2
Luiz Dias <luiz.dias@sumup.com>
2021-12-06 20:00:15 -0300

    Adding logic of taxes and validation for internation transfer

---
+++ aula2/internationalTransfer.js
@@ -0,0 +1,35 @@
+import { Account } from "./account";
+
+export function internationalTransfer(payer, receiver, transferAmount) {
+    validateAmountLimit(transferAmount)
+
+    const tax = calculateTax(transferAmount)
+
+    validatePayerAmount(payer, transferAmount, tax)
+
+    const updatedPayerAccount = new Account(payer.id, payer.balance - transferAmount - tax)
+    const updatedReceiverAccount = new Account(receiver.id, receiver.balance + transferAmount)
+
+    return [updatedPayerAccount, updatedReceiverAccount]
+
+}
+
+function validateAmountLimit(transferAmount) {
+    if (transferAmount < 1000 || transferAmount > 9999) {
+        throw new Error(`Transfer amount is invalid: ${transferAmount}`);
+    }
+}
+
+function validatePayerAmount(payer, transferAmount, tax) {
+    if (payer.balance < transferAmount + tax) {
+        throw new Error(`Insufficient funds`);
+    }
+}
+
+function calculateTax(amount) {
+    const fixedTax = 100
+    if (amount <= 5000) {
+        return amount * 0.05 + fixedTax
+    }
+    return amount * 0.1 + fixedTax
+}
